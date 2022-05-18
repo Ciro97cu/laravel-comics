78b3e57102bbdee025a95aac89feced902b4a74b
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-18 17:04:16 +0200

    added header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,65 @@
+<header>
+    <div class="container py-3">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="logo_wrapper">
+            <img src="../images/dc-logo.png" alt="logo-dc" />
+          </div>
+        </div>
+        <div class="col-sm-8">
+          <ul class="row justify-content-between">
+            <li class="col-auto">
+              <a href="#">
+                CHARACTERS
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                COMICS
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                MOVIES
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                TV
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                GAMES
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                COLLECTIBLES
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                VIDEOS
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                FANS
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                NEWS
+              </a>
+            </li>
+            <li class="col-auto">
+              <a href="#">
+                SHOP
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
