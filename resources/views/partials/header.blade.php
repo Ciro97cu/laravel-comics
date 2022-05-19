41cbb72bb6947d5a4826c185de91358193cfa5cb
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-19 21:07:22 +0200

    added section + added link dinamically

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,64 +1,34 @@
 <header>
+    <section class="dc_head">
+      <div class="container-sm">
+        <div class="row justify-content-end ">
+          <div class="col-auto text-white">
+            <span class="pe-5">DC POWER&trade;VISA	&reg;</span>
+            <span>ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></span>
+          </div>
+        </div>
+      </div>
+    </section>
     <div class="container py-3">
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-1">
           <div class="logo_wrapper">
             <img src="/images/dc-logo.png" alt="logo-dc" />
           </div>
         </div>
-        <div class="col-sm-8">
+        <div class="col-sm-9">
           <ul class="row justify-content-between">
-            <li class="col-auto">
-              <a href="#">
-                CHARACTERS
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                COMICS
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                MOVIES
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                TV
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                GAMES
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                COLLECTIBLES
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                VIDEOS
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                FANS
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                NEWS
-              </a>
-            </li>
-            <li class="col-auto">
-              <a href="#">
-                SHOP
-              </a>
-            </li>
+            @foreach ($navLink as $link)
+              <li class="col-auto">
+                <a href="#">
+                  {{$link}}
+                </a>
+              </li>
+            @endforeach
           </ul>
+        </div>
+        <div class="col-sm-2">
+          <input type="text" placeholder="Search &#xf002;">
         </div>
       </div>
     </div>
