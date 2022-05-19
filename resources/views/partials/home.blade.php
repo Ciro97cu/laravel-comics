17e0c62acdfbb5941fa8c973f52f9ebf8e49b342
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-19 21:08:16 +0200

    added tag_series + added index in foreach

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -1,25 +1,26 @@
 @extends('../index')
 
 @section('main')
-    <main>
-        <div class="container_cards py-4">
+<main>
+    <div class="container_cards py-4">
         <div class="container-sm">
+            <div class="tag_series">CURRENT SERIES</div>
             <div class="row">
-            @foreach ($array as $comic)
-            <div class="col-sm-2">
-                <div class="wrapper_img">
-                <a href="/card">
-                    <img class="pt-5 pb-3" src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
-                </a>
+                @foreach ($array as $index => $comic)
+                <div class="col-sm-2">
+                    <div class="wrapper_img">
+                        <a href="/card/{{$index}}">
+                            <img class="pt-5 pb-3" src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
+                        </a>
+                    </div>
+                    <p>{{ $comic["series"] }}</p>
                 </div>
-                <p>{{ $comic["series"] }}</p>
-            </div>
-            @endforeach
+                @endforeach
             </div>
             <div class="row mt-5 justify-content-center">
-            <div class="col-auto tag_load">LOAD MORE</div>
+                <div class="col-auto tag_load">LOAD MORE</div>
             </div>
         </div>
-        </div>
-    </main>
+    </div>
+</main>
 @endsection
