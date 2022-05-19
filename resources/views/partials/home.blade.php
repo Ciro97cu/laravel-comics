ae5a550893532b3cdc85a064bd997fb29bf5c83b
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-19 08:59:20 +0200

    reduced code

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -1,5 +1,25 @@
-@extends('../index');
+@extends('../index')
 
 @section('main')
-    @include("partials/main")
+    <main>
+        <div class="container_cards py-4">
+        <div class="container-sm">
+            <div class="row">
+            @foreach ($array as $comic)
+            <div class="col-sm-2">
+                <div class="wrapper_img">
+                <a href="/card">
+                    <img class="pt-5 pb-3" src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
+                </a>
+                </div>
+                <p>{{ $comic["series"] }}</p>
+            </div>
+            @endforeach
+            </div>
+            <div class="row mt-5 justify-content-center">
+            <div class="col-auto tag_load">LOAD MORE</div>
+            </div>
+        </div>
+        </div>
+    </main>
 @endsection
--- resources/views/partials/homeCard.blade.php
+++ resources/views/partials/homeCard.blade.php
@@ -1,5 +1,16 @@
 @extends('../index');
 
 @section('main')
-    @include("partials/mainCard")
+    <main>
+        <div class="container_cards py-4">
+        <div class="container-sm">
+            <div class="text-white">
+            <a href="/">Back</a>
+            </div>
+            <div class="row mt-5 justify-content-center">
+            <div class="col-auto tag_load">LOAD MORE</div>
+            </div>
+        </div>
+        </div>
+    </main>
 @endsection
